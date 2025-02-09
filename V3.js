3872f85d8dbb96b831a01dc9fb10e555b67ff8b1
Pradyumn Chauhan <140681043+PradyumnChauhan@users.noreply.github.com>
2025-02-09 16:43:46 +0530

    V3.js

--- V3.js
+++ V3.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 import fetch from 'node-fetch';
-
+import dotenv from 'dotenv';
 // MongoDB connection
-mongoose.connect('mongodb+srv://kunnuv3:rpBiLsTBk6fiYOH7@cluster0.a0fu5.mongodb.net/V3')
+mongoose.connect(process.env.MONGODB_URL)
   .then(() => {
     console.log('Connected to MongoDB');
     sendTelegramMessage('MongoDB Connection', 'Successfully connected to MongoDB');
@@ -13,8 +13,8 @@ mongoose.connect('mongodb+srv://kunnuv3:rpBiLsTBk6fiYOH7@cluster0.a0fu5.mongodb.
   });
 
 // Telegram configuration
-const botToken = '7595498223:AAE89tHasgl7R8WGBznjpYGzDY14gDDvb7I';
-const chatId = '-1002257707907';
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
 
 // Telegram message sender
 const sendTelegramMessage = async (status, details) => {
@@ -123,7 +123,7 @@ const getOptionsData = async () => {
        headers : {
         'accept': '*/*',
         'accept-language': 'en-US,en;q=0.7',
-        'cookie': 'nsit=iciqmlYvi4LOLWB-KR9kqDbd; AKA_A2=A; *abck=B93F482623BEF9F317FBE50115BA01EF~0~YAAQMyEPFz7HHtuRAQAA0PvXFwzLiLN+/9D/rG4RZjtNlPz6OWF0PpMDm8uy5DIACAQbn62ziE2A6KxpNBD7Vtm4uyfMwETm40iKl7epVz+LVbsZpP87Wj8QWVaKdT/R8L+e1MpS2/LAo0kG6j0OLhUjjHY2ufV2/kPDAFjyYoCScwlYQTJAq5GmouwCF80SIA4gmnTYgzBBvQhX4QZqDCVAiVJzWcKmA656UtnRwcwAbEr80hLFp3u/TzztzveYeACTl6qRbrN605nrJIxCK7hCHODW8PBBMhahQdp2uWzHdIjd0osbaxXpX/trsm9U33Bv0sACBKTUJyrnhHQVzjwXwlfR5Z1d2QB6mO5BiHgkVhe70teBpjgfc7xtcOYMmTVeWLyz5wnpzZyVx331fFTwXuLcLUhHcYQ=~-1~-1~-1; defaultLang=en; ak*bmsc=5E6E3FD57D3C5DCBFB527F924EBEFCC0~000000000000000000000000000000~YAAQMyEPF2/HHtuRAQAAoQHYFxnG/z6wE7i/CMmFb+PTXI6oVJ3tU7jzhaZyh8IKgYxDNLhFMhhaOEiwIbQsgTCJG05CEZNvXW7uQ6uSn7on2ZXfxbIX1XjVy5UtsFd9z8NyYX5ByZK5b7zq3v0Fedfl6NHqlGgNkEpqE4VwxDaBEUl7d8oKoC2kE6IWVVHtw3yNBJHtJ7HhpSuLQeSwOXI3xvh58S4SRai8ryemSigKQZVRcSteA6i4NHzkF3DpB0OVXD1WZnC35bEYOvY+dQ95N0sFNUAtk1IZJormTaX8VDIrU23dPZHMRTAZ/iK1cR+GTsDA7ziQB1RPP1pfORJsP9n0AtYelZWDsU9iXeKg0k5W7HsoziqAaa+6KvmOM8e9d2dwVNttczRSmAcqPmhlomLmZ7QWemsNfhCugm+FkuKyL59E0IGHLAQNZf+KvV0HhopWlofce12A9W1o; nseappid=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGkubnNlIiwiYXVkIjoiYXBpLm5zZSIsImlhdCI6MTcyNjk3ODY4NCwiZXhwIjoxNzI2OTg1ODg0fQ.nfzkgPbGlZfK9hj_qX_GTp-orMKkSLssYPf42HIatGQ; bm_sz=FC2F4A1DE1A7B81D513F8C80CBA8FFA2~YAAQMyEPF5zFH9uRAQAA3HTzFxmisGpRU76NRKVS2yc06hfr+tTS/mhzxzOONh7Y++gPLKLqeVq3CnZ9VuNmy2rjCFZjoLl4S8Fm2fKgAXXCjyQxF3Onv7xzGOxgTCgvJA+fQkEakuAZi7gbgApn52VrVYyJswuaPBgOAmlZZlBTy419N3dc7r7EukAaUMsf/Y+SHOoKH2AvhbOz4ObbHZ9OmhCZ7+KhncEtyjOQDJrdw6t34lvdmL8MClkZ71ij7aDrD+28lcSrRFuHIiIAFMKVtV6Im2flnfqV3HDraSlzODnJjGLgSGonWiQ5iCNDMnZIl4MEvXOQOOJ5OjATrPZ4vwKI8qg6LOdj7owMyKN+U+rbifo+HvBIVhh/8HaqLa23IOzdNWQT5ZAPPVh3eLp1pWuytbqSMrnile+4~4601396~3158321; bm_sv=FBC4F9C5CD7CB9AEDFED1AE748B021D1~YAAQMyEPF+nFH9uRAQAAfHjzFxnqYjjQu0Xik3IzBGMLX8/Ao7cMOjuSWr/Ejo6rXJupH5BM0/xEZtilBlZypkKQfZLSfUatGsZ7/G5fueHEahjMogpz5+2jmt9R77p8aou5wDN0gZfyWmz2jwfcKFTqCnxrzO9zFBHAW8pXjtuPRNDCHgWU3kwAqrAfTx1bA1xPn9rfL167O+sP4ccc7wd48Es69KiUoASHtywV59MpAf6TOwlCEJzyG0LM5BfGC0pSLg==~1',
+        'cookie': process.env.NSE_API,
         'priority': 'u=1, i',
         'referer': 'https://www.nseindia.com/option-chain',
         'sec-ch-ua': '"Brave";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
